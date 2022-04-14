42a952e9b004506a30ba6b13ff3d12e2c2e5150f
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-04-14 21:17:03 +0530

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-gray-800">Nextjs Tailwind</h1>
+      <h1 className="text-5xl font-bold text-gray-800">
+        Nextjs Tailwind Wordpress
+      </h1>
     </div>
   );
 };
