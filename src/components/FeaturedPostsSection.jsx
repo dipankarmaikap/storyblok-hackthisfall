cf1a158c52893f671611794869b9bc64476aa70e
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-01-09 18:29:42 +0530

    Starting point with updated dummy data

--- src/components/FeaturedPostsSection.jsx
+++ src/components/FeaturedPostsSection.jsx
@@ -3,17 +3,20 @@ import { posts } from "~/lib/static-data";
 
 export default function FeaturedPostsSection() {
   return (
-    <section className="my-8">
+    <section id="FeaturedPostsSection" className="FeaturedPostsSection my-8">
       <p className="p-4 font-serif text-lg">Featured posts</p>
       <div className="recent-posts">
         {posts.map((post) => (
-          <article key={post} className="border-b border-gray-700 p-4 lg:py-6">
+          <article
+            key={post.id}
+            className="border-b border-gray-700 p-4 lg:py-6"
+          >
             <h2>
               <Link
                 className="post text-5xl lg:text-7xl font-black"
-                href={`/blogs/${post?.toLowerCase().replace(/\s/g, "-")}`}
+                href={post.full_slug}
               >
-                {post}
+                {post.name}
               </Link>
             </h2>
           </article>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,11 @@ import {
   StoryblokComponent,
   useStoryblokState,
 } from "@storyblok/react";
-import { Fragment } from "react";
-import FeaturedPostsSection from "~/components/FeaturedPostsSection";
-import FeaturedProjectsSection from "~/components/FeaturedProjectsSection";
-import HeroSection from "~/components/HeroSection";
 
 export default function HomePage({ story }) {
+  story = useStoryblokState(story);
   // console.log(story);
-  return (
-    <Fragment>
-      <HeroSection />
-      <FeaturedPostsSection />
-      <FeaturedProjectsSection />
-    </Fragment>
-  );
+  return <StoryblokComponent blok={story.content} />;
 }
 export async function getStaticProps() {
   // home is the default slug for the homepage in Storyblok
@@ -32,7 +23,7 @@ export async function getStaticProps() {
   return {
     props: {
       key: "home-page",
-      // story: data ? data.story : false,
+      story: data ? data.story : false,
     },
     revalidate: 60,
   };
