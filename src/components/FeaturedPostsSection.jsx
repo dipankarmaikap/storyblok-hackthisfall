f3518acc49bedad8f5a7a48eb256eb3bdc0ff467
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-01-09 18:01:21 +0530

    More clean up the starting point

--- src/components/FeaturedPostsSection.jsx
+++ src/components/FeaturedPostsSection.jsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import { posts } from "~/lib/static-data";
-import PostItem from "./PostItem";
 
 export default function FeaturedPostsSection() {
   return (
@@ -7,11 +7,16 @@ export default function FeaturedPostsSection() {
       <p className="p-4 font-serif text-lg">Featured posts</p>
       <div className="recent-posts">
         {posts.map((post) => (
-          <PostItem
-            title={post}
-            href={`/blogs/${post?.toLowerCase().replace(/\s/g, "-")}`}
-            key={post}
-          />
+          <article key={post} className="border-b border-gray-700 p-4 lg:py-6">
+            <h2>
+              <Link
+                className="post text-5xl lg:text-7xl font-black"
+                href={`/blogs/${post?.toLowerCase().replace(/\s/g, "-")}`}
+              >
+                {post}
+              </Link>
+            </h2>
+          </article>
         ))}
       </div>
     </section>
