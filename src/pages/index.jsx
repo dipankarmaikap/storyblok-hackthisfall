2926283cfbaf04aef1cb06052363a6a0f31bc140
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-01-09 14:16:52 +0530

    updated starting point

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,15 @@
+import {
+  getStoryblokApi,
+  StoryblokComponent,
+  useStoryblokState,
+} from "@storyblok/react";
 import { Fragment } from "react";
 import FeaturedPostsSection from "~/components/FeaturedPostsSection";
 import FeaturedProjectsSection from "~/components/FeaturedProjectsSection";
 import HeroSection from "~/components/HeroSection";
 
-export default function HomePage() {
+export default function HomePage({ story }) {
+  // console.log(story);
   return (
     <Fragment>
       <HeroSection />
@@ -11,4 +17,23 @@ export default function HomePage() {
       <FeaturedProjectsSection />
     </Fragment>
   );
+}
+export async function getStaticProps() {
+  // home is the default slug for the homepage in Storyblok
+  let slug = "home";
+
+  // load the draft version
+  let sbParams = {
+    version: "draft", // or 'published'
+  };
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  return {
+    props: {
+      key: "home-page",
+      // story: data ? data.story : false,
+    },
+    revalidate: 60,
+  };
 }
